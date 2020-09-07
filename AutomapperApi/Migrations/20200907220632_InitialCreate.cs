6b384a4d1ba5cc14939b03ae3c24cda9f728c4e1
Emerson Bilieri <ebilieri@hotmail.com>
2020-09-07 20:45:34 -0300

    Remove ignored files

---
+++ AutomapperApi/Migrations/20200907220632_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AutomapperApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", nullable: true),
+                    Address = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
